b45c56a98c5842d60d529924b3de844bf162fb93
udit <yudit1994@gmail.com>
2022-06-11 11:23:48 +0530

    second

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 const db= mongoose.connection;
 
 
-mongoose.connect('mongodb://localhost:27017/hospital-api');
+mongoose.connect(process.env.db ||'mongodb://localhost:27017/hospital-api');
 
 module.exports=db;
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = 8000;
+const port = process.env.PORT || 8000;
 const app = express();
 
 //use body parser
